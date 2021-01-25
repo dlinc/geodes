3dcc42f3f0bfe2efac6f7026afbd921c025cb10a
dlinc <46935057+dlinc@users.noreply.github.com>
2021-01-24 16:10:42 -0800

    more config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
+    "plugin:vue/base",
     'plugin:vue/vue3-essential',
     'eslint:recommended',
   ],
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 // vue.config.js
 
 //process.env.VUE_APP_VERSION = require('./package.json').version
-
+//console.log("All config ENV =",process.env)
 module.exports = {
-  publicPath: (process.env.runPath ? process.env.runPath : '/' ),
-  outputDir: (process.env.runDir ? process.env.runDir : 'dist')
-  
-  //process.env.NODE_ENV === 'production'
-  //  ? '/production-sub-path/'
-  //  : '/yy'
+  //publicPath: (process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : '/' ),
+  //publicPath: "/geodes", 
+  outputDir: (process.env.VUE_APP_BUILD_DIR ? process.env.VUE_APP_BUILD_DIR : 'dist'),
+  publicPath: process.env.NODE_ENV === 'production'
+    ?  process.env.VUE_APP_BASE_URL 
+    : '/'
 
 }
