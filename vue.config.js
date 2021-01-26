55b181d4b76e9d3f7c1c250b7cb436a0fb2d3c05
dlinc <46935057+dlinc@users.noreply.github.com>
2021-01-26 13:30:07 -0800

    config for deploy

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,10 @@
 
 //process.env.VUE_APP_VERSION = require('./package.json').version
 //console.log("All config ENV =",process.env)
-module.exports = {
-  //publicPath: (process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : '/' ),
-  //publicPath: "/geodes", 
+module.exports = { 
   outputDir: (process.env.VUE_APP_BUILD_DIR ? process.env.VUE_APP_BUILD_DIR : 'dist'),
   publicPath: process.env.NODE_ENV === 'production'
-    ?  process.env.VUE_APP_BASE_URL 
-    : '/'
+    ?  process.env.VUE_APP_PRODUCTION_BASE_URL 
+    : process.env.VUE_APP_BASE_URL
 
 }
