63d04e93d0c39336200069d5f58ee015a5ab6473
dlinc <46935057+dlinc@users.noreply.github.com>
2021-04-09 11:45:01 -0600

    modal structure

---
+++ src/views/components/showCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>{{ title }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    <ion-page>
+     {{ content }}
+    </ion-page>
+  </ion-content>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'showCard',
+  props: {
+    title: { type: String, default: 'Title Geode' },
+  },
+  data(data) {
+    return {
+      content: data.content,
+    }
+  },
+  components: { IonContent, IonHeader, IonTitle, IonToolbar, IonPage }
+});
+</script>
--- src/views/list.vue
+++ src/views/list.vue
@@ -8,7 +8,7 @@
             <ion-item 
               v-for="ode in odes" 
               v-bind:key="ode.sid"
-              @click.stop="clickView(`${ode.sid}`)"
+              @click.stop="clickView(ode)"
               >
               <ion-item-divider class="ion-padding">
                       <ion-thumbnail slot="start">
@@ -19,12 +19,20 @@
               </ion-item-divider>   
             </ion-item> 
         </ion-list>
+        <ion-modal
+            :data="data"
+            :is-open="isOpenRef"
+            css-class="my-custom-class"
+            @onDidDismiss="setOpen(false)"
+          >This is a test here {{ data }}</ion-modal>
     </ion-content>
   </ion-page>
 </template>
 <script>
-import { defineComponent} from 'vue';
+import { defineComponent, ref} from 'vue';
 import Odes from "../factories/odeFactory"
+import ShowCard from "./components/showCard"
+
 import {
   IonContent,
   IonPage,
@@ -35,20 +43,29 @@ import {
   IonItemDivider,
   IonThumbnail,
   IonImg,
+  IonModal,
+  modalController
 } from '@ionic/vue';
 
 export default defineComponent({
   components: { 
     IonContent, 
-  IonPage, 
-  IonList, 
-  IonLabel, 
-  IonItem, 
-  IonListHeader,
-  IonItemDivider,
-  IonThumbnail,
-  IonImg,
-  } ,
+    IonPage, 
+    IonList, 
+    IonLabel, 
+    IonItem, 
+    IonListHeader,
+    IonItemDivider,
+    IonThumbnail,
+    IonImg,
+    IonModal
+    },
+  setup() {
+    const isOpenRef = ref(false);
+    const setOpen = (state) => isOpenRef.value = state;
+    const data = { content: 'New Content XXX' };
+    return { isOpenRef, setOpen, data }
+  },
   data () {
     let odes=Odes.getOdes().stack
     console.log("roll LIST view ",odes)
@@ -59,20 +76,33 @@ export default defineComponent({
     }
   },
   methods: {
-      clickView: function (id) {
-        alert("clicked test "+id)
-      }
+      clickView: function (ode) {
+        //this.setOpen(true)
+        this.openModal(ode)
+        console.log("clicked test ",ode.title)
+      },
+      async openModal(ode) {
+        const modal = await modalController
+          .create({
+            component: ShowCard,
+            cssClass: 'my-custom-class',
+            componentProps: {
+              title: ode.title
+            },
+          })
+        return modal.present();
+    },
   },
   beforeMount(){
-   // console.log("LIST before mount",this.odes)
+   console.log("LIST before mount",this.odes)
   },
   beforeUpdate(){
     //this.odes==Odes.getOdes().stack
-    //console.log("LIST before update",this.odes)
+    console.log("LIST before update",this.odes)
     return this.odes
   },
   updated () {
-    //console.log("LIST rendered event",this.odes)
+    console.log("LIST updated event",this.odes)
     //this.loading.dismiss()
   }
   //, Loaders }
