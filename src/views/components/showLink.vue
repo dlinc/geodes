5df6030f27c5a133754d5ee3e05c374fa25685ac
dlinc <46935057+dlinc@users.noreply.github.com>
2021-05-03 15:24:16 -0600

    add 360s

--- src/views/components/showLink.vue
+++ src/views/components/showLink.vue
@@ -4,11 +4,10 @@
         <ion-header>
         <ion-title class="ion-float-left ion-padding">Title here</ion-title>
         </ion-header>
-
             <iframe class="webgl"
                 allowfullscreen
                 frameborder="0"
-                src="http://localhost:8081/firstGl.html">
+                :src="`${webGlPage}`">
             </iframe>
     </ion-content>
   </ion-page>
@@ -30,20 +29,22 @@ export default defineComponent({
     IonContent,IonPage,IonHeader, IonTitle
   },
   setup() {
-/*
-     onIonViewWillEnter(() => {
-      allLocation = getLocData()
-      console.log('page did enter', allLocation);
-      return allLocation
-    });*/
 
     const route = useRoute();
     // eslint-disable-next-line no-unused-vars
     const { x1,x2,y1,y2, gleo } = route.query;
-    console.log("Params in -", route.query,x1,x2,y1,y2, gleo );
-    //allLocation = getLocData()
+    let webGlPage;
+    if (gleo) {
+      if (gleo==="1") { webGlPage = "http://localhost:8081/showPanorama.html?img=20191117_231638(0).jpg"; }
+      if (gleo==="2") { webGlPage = "http://localhost:8081/showPanorama.html?img=20171013_220258.jpeg"; }
+    }
+    //const webGlPage = "http://localhost:8081/firstAframe.html";
+    //const webGlPage = "http://localhost:8081/firstGl.html";
+    
+
+    //allLocation = getLocDataP()
     //let geo = Geo.getGeo();
-    return { x1,x2,y1,y2, gleo }
+    return { x1,x2,y1,y2, gleo, webGlPage }
 },
 data(){
 
@@ -56,6 +57,6 @@ data(){
 .webgl {
   xborder:1px red solid;
   width:100%;
-  height:100%;
+  height:90%;
 }
 </style>
